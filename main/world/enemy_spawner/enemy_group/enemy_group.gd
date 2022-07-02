c263b12ad8230f15120894a6f61e4e905156361a
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2022-07-01 21:40:07 -0500

    Added enemies and text level parser
    Moved around files for organization.

---
+++ main/world/enemy_spawner/enemy_group/enemy_group.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name EnemyGroup
+
+var enemy_scene: PackedScene
+var count: int
+var delay: float
---
+++ main/world/enemy_spawner/round/round.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Round
+
+var spawn_times: Array[float]
+var enemy_groups: Array[EnemyGroup]
